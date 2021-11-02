52b565327ebe3e27e0015cc28f41cf5289d3fa13
David Gomez <jose.david.gomez@segurosbolivar.com.co>
2021-11-02 15:23:43 -0500

    Commit Pruebas APP

--- ColinaApplication/ColinaApplication/ScriptsViews/Shared/AlertasLayout.js
+++ ColinaApplication/ColinaApplication/ScriptsViews/Shared/AlertasLayout.js
@@ -1,13 +1,33 @@
-﻿
-$(function Alertas() {
-    var connect = $.connection.sharedhub;
+﻿var idleTime = 0;
 
+$(function Alertas() {
+
+    //Increment the idle time counter every minute.
+    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
+    //Zero the idle timer on mouse movement.
+    $(this).mousemove(function (e) {
+        idleTime = 0;
+    });
+    $(this).keypress(function (e) {
+        idleTime = 0;
+    });
+
+    //HUB GLOBAL
+    var connect = $.connection.sharedhub;
     Llama_Metodos(connect);
 
     $.connection.hub.start().done(function () {
         Registra_Eventos(connect);
     });
 });
+
+//FUNCION PARA CERRAR SESION DESPUES DE 5 MINUTOS DE INACTIVIDAD
+function timerIncrement() {
+    idleTime = idleTime + 1;
+    if (idleTime > 4) { // 5 minutes
+        document.getElementById("CerrarSesion").click();
+    }
+}
 
 function Registra_Eventos(connect) {
     $('#botonCierre').click(function () {
