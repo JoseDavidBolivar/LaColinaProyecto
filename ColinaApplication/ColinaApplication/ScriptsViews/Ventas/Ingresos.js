bef709f503393ab105f33d0f5d37d0ff1cf331a5
David Gomez <jose.david.gomez@segurosbolivar.com.co>
2022-03-12 17:40:31 -0500

    Arreglo Cagada

--- ColinaApplication/ColinaApplication/ScriptsViews/Ventas/Ingresos.js
+++ ColinaApplication/ColinaApplication/ScriptsViews/Ventas/Ingresos.js
@@ -1,4 +1,4 @@
-﻿reImprimir$(document).ready(function () {  
+﻿$(document).ready(function () {  
     $(".TablaList").DataTable({
         "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json",
     });
